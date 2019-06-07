2535a79d22afb975f3db21b030eeeaa8b7a1200b
t430s <t430s@noviqlaptop97>
2019-06-07 18:45:38 +0300

    final version

--- ChatServer/src/com/fmi/spo/chatserver/ClientHandler.java
+++ ChatServer/src/com/fmi/spo/chatserver/ClientHandler.java
@@ -61,7 +61,7 @@ public class ClientHandler extends Thread {
 		}
 	}
 
-	public synchronized void writeMessage(ServerMessageWrapper message) {
+	public void writeMessage(ServerMessageWrapper message) {
 		try {
 			this.clientOutput.writeObject(message);
 		} catch (Exception e) {
